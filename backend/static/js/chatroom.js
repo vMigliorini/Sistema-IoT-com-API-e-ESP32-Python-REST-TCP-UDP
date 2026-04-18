0a5a2a2fb6de02ba01a6781f20149659f7841a51
Migliorini <vicentemigliorini2007@gmail.com>
2026-04-18 13:50:42 -0300

    criado o inicio da implementação de adicionar nova chat room e inicio da implementação de adicionar mensagens

--- backend/static/js/chatroom.js
+++ backend/static/js/chatroom.js
@@ -9,3 +9,41 @@ window.onload = async function() {
     document.getElementById("nome-user-perfil").innerHTML = user.nome
     document.getElementById("cargo-user-perfil").innerHTML = user.cargo
 }
+
+async function adicionar_chat(event){
+    event.preventDefault();
+}
+
+async function enviar_mensagem(event) {
+    event.preventDefault();
+    var form = document.getElementById("form-mensagem-digitada")
+    var form_mensagem = new FormData(form)
+
+    const mensagem = form_mensagem.get('conteudo-mensagem')
+
+    var chat = `
+                    <div class="mensagem">
+                        <div class="icone-user">
+                            <i class="fa-solid fa-circle-user"></i>
+                        </div>
+                        <div class="conteudo-mensagem">
+                            <div class="nome-autor">
+                                User
+                            </div>
+                            <div class="texto-autor">
+                                oi
+                            </div>
+                        </div>
+                    </div>
+    `
+
+    const response = await fetch("/chat_rooms", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+            // PRECISA CRIAR A CHAT ROOM ANTES DE ENVIAR A MENSAGEM DO CHAT
+        })
+    })
+}
