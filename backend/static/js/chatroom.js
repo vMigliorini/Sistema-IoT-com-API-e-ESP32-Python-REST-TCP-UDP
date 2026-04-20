0a0b4d8d92bea6839b0dd97ac968904d7745e2ed
Migliorini <vicentemigliorini2007@gmail.com>
2026-04-20 11:19:43 -0300

    criado pop up de adicionar novo groupchat

--- backend/static/js/chatroom.js
+++ backend/static/js/chatroom.js
@@ -1,5 +1,6 @@
 window.onload = async function() {
-
+    const dialog = document.getElementById('pop_up_adiocionar_chat');
+    dialog.close()
     const dados_user = await fetch("/me")
     const user = await dados_user.json()
     if (!user.ok){
@@ -10,40 +11,14 @@ window.onload = async function() {
     document.getElementById("cargo-user-perfil").innerHTML = user.cargo
 }
 
-async function adicionar_chat(event){
-    event.preventDefault();
+
+
+async function abrir_popup_add_chat(){
+    const dialog = document.getElementById('pop_up_adiocionar_chat');
+    dialog.showModal()
 }
 
-async function enviar_mensagem(event) {
-    event.preventDefault();
-    var form = document.getElementById("form-mensagem-digitada")
-    var form_mensagem = new FormData(form)
-
-    const mensagem = form_mensagem.get('conteudo-mensagem')
-
-    var chat = `
-                    <div class="mensagem">
-                        <div class="icone-user">
-                            <i class="fa-solid fa-circle-user"></i>
-                        </div>
-                        <div class="conteudo-mensagem">
-                            <div class="nome-autor">
-                                User
-                            </div>
-                            <div class="texto-autor">
-                                oi
-                            </div>
-                        </div>
-                    </div>
-    `
-
-    const response = await fetch("/chat_rooms", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body:JSON.stringify({
-            // PRECISA CRIAR A CHAT ROOM ANTES DE ENVIAR A MENSAGEM DO CHAT
-        })
-    })
+async function fechar_popup_add_chat(params) {
+    const dialog = document.getElementById('pop_up_adiocionar_chat');
+    dialog.close()
 }
